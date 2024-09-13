d1d901f4e202324a3a8683becdd34f1b9cd58b1e
Mayank gudadhe CI-35 <88714407+MayankgudadheCI@users.noreply.github.com>
2024-09-13 13:37:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 variable "access_key" {}
 variable "secret_access_key" {}
-variable "existing_security_group_id" {
-  description = "sg-0775b034d32553d56"
-}
 
 provider "aws" {
   access_key = var.access_key
@@ -14,7 +11,7 @@ resource "aws_instance" "machine" {
   ami             = "ami-0d1e92463a5acf79d"
   instance_type   = "t2.micro"
   key_name        = "deploy"
-  security_group_ids = [var.existing_security_group_id]
+  security_group_ids = "sg-0775b034d32553d56"
   user_data = <<-EOF
     #!/bin/bash
     cd /mnt
