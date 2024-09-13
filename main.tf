18db949310ec33679cd19ffc2227cf844c9b1f05
Mayank gudadhe CI-35 <88714407+MayankgudadheCI@users.noreply.github.com>
2024-09-13 14:03:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,35 @@ provider "aws" {
   secret_key = var.secret_access_key
   region     = "ap-south-1"
 }
+data "aws_vpc" "default" {
+  default = true
+}
+resource "aws_security_group" "sec_a" {
+  vpc_id = aws_vpc.default.id
+  tags = {
+    Name = "sec_a"
+  }
+  
+  ingress {
+    description = "SSH_ALL"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "machine" {
   ami             = "ami-0d1e92463a5acf79d"
   instance_type   = "t2.micro"
   key_name        = "deploy"
+  security_group_id = aws_security_group.sec_a.id
   user_data = <<-EOF
     #!/bin/bash
     cd /mnt
