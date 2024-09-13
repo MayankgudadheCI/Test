b53fe589e1b88c9ae40247037e196d32c3bf35f0
Mayank gudadhe CI-35 <88714407+MayankgudadheCI@users.noreply.github.com>
2024-09-13 12:43:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_access_key
   region     = "ap-south-1"
+}
 resource "aws_instance" "machine" {
   ami             = "ami-0d1e92463a5acf79d"
   instance_type   = "t2.micro"
