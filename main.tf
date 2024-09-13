64f7cbfad8f7abebaa6f99a2853abc551c7e3c96
Mayank gudadhe CI-35 <88714407+MayankgudadheCI@users.noreply.github.com>
2024-09-13 15:21:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,43 @@ provider "aws" {
 data "aws_vpc" "default" {
   default = true
 }
+resource "aws_security_group" "custom" {
+  name        = "custom-security-group"
+  description = "Custom security group in the default VPC"
+  vpc_id      = data.aws_vpc.default.id
+
+  # Ingress rules
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Egress rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "custom-security-group"
+  }
+}
 resource "aws_instance" "machine" {
   ami             = "ami-0d1e92463a5acf79d"
   instance_type   = "t2.micro"
   key_name        = "deploy"
+  security_group_id = aws_security_group.custom.id
   user_data = <<-EOF
     #!/bin/bash
     cd /mnt
