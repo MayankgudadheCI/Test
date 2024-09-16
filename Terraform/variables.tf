7923a71c4073173fa2252f53f76d1eb0de93e220
MayankgudadheCI <mayank@gmail.com>
2024-09-16 19:00:06 +0530

    Update var

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -7,3 +7,24 @@ variable "secret_access_key" {
   description = "AWS secret access key"
   type        = string
 }
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key name for the EC2 instance"
+  type        = string
+}
