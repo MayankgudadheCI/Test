b4ea87364df97a6638f193de38d436058785ab9a
Mayank gudadhe CI-35 <88714407+MayankgudadheCI@users.noreply.github.com>
2024-09-16 17:22:15 +0530

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "secret_access_key" {
+  description = "AWS secret access key"
+  type        = string
+}
