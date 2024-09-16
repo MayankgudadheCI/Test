2eed04b85ba68dc77eadc623a95073a074a71059
Mayank gudadhe CI-35 <88714407+MayankgudadheCI@users.noreply.github.com>
2024-09-16 17:33:22 +0530

    Delete Terraform/modules/ec2-instance/backend.tf

--- Terraform/modules/ec2-instance/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "deploy-mayank-mumbai"   
-    key            = "terraform/state/terraform.tfstate"  
-    region         = "ap-south-1"            
-  }
-}
-
