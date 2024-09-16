780df4d7da4433d3682ecea15830db12a64f6d56
Mayank gudadhe CI-35 <88714407+MayankgudadheCI@users.noreply.github.com>
2024-09-16 16:02:28 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,1 +1,10 @@
 
+variable "access_key" {
+  description = "AWS access key for API calls"
+  type        = string
+}
+variable "secret_access_key" {
+  description = "AWS secret access key for API calls"
+  type        = string
+}
+
