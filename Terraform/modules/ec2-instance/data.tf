c58afa2a61678ea68771964ae5425dd4ad414d5a
MayankgudadheCI <mayank@gmail.com>
2024-09-17 19:14:18 +0530

    Updated

--- Terraform/modules/ec2-instance/data.tf
+++ Terraform/modules/ec2-instance/data.tf
@@ -2,5 +2,13 @@ data "aws_vpc" "default" {
   default = true
 }
 data "template_file" "user_data" {
-  template = file("user_data.tpl")
+  template = file("${path.module}/user_data.tpl")
+
+  vars = {
+    aws_region        = ap-south-1
+    bucket_name       = deploy-mayank-mumbai
+    object_key        = LoginWebApp
+    access_key        = var.access_key
+    secret_access_key = var.secret_access_key
+  }
 }
