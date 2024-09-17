cbf6e54c1095a788ea37921a869bf7b6a4f5ee63
MayankgudadheCI <mayank@gmail.com>
2024-09-17 19:06:31 +0530

    Updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,4 @@
-data "template_file" "user_data" {
-  template = file("${path.module}/user_data.tpl")
-}
+
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_access_key
--- Terraform/modules/ec2-instance/data.tf
+++ Terraform/modules/ec2-instance/data.tf
@@ -1,3 +1,6 @@
 data "aws_vpc" "default" {
   default = true
 }
+data "template_file" "user_data" {
+  template = file("user_data.tpl")
+}
