b14a439935b6a1caef438e1b43a98cf87130395c
MayankgudadheCI <mayank@gmail.com>
2024-09-17 12:59:28 +0530

    update tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_access_key
-  region     = "ap-south-1"
+  region     = var.region
 }
 
 module "ec2-instance" {
-  source        = "./modules/ec2-instance"
-  ami           = "ami-0d1e92463a5acf79d"
-  instance_type = "t2.micro"
-  key_name      = "deploy"
-  access_key    = var.access_key
+  source             = "./modules/ec2-instance"
+  ami                = var.ami
+  instance_type      = var.instance_type
+  key_name           = var.key_name
+  access_key         = var.access_key
   secret_access_key = var.secret_access_key
 }
 
@@ -19,8 +19,4 @@ output "instance_id" {
 
 output "instance_public_ip" {
   value = module.ec2-instance.public_ip
-}
-
-output "security_group_id" {
-  value = module.ec2-instance.security_group_id
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,10 +1,40 @@
+# variables.tf
+
 variable "access_key" {
-  description = "AWS access key ID"
+  description = "The AWS access key"
   type        = string
+  sensitive   = true
 }
 
 variable "secret_access_key" {
-  description = "AWS secret access key"
+  description = "The AWS secret access key"
   type        = string
+  sensitive   = true
 }
+
+variable "region" {
+  description = "The AWS region to deploy the resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "ami" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0d1e92463a5acf79d"
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair to use"
+  type        = string
+  default     = "deploy"
+}
+
+
 
