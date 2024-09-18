018fb1150d097e17cd960fba0762b446ca173413
MayankgudadheCI <mayank@gmail.com>
2024-09-18 11:38:10 +0530

    Updated

--- Terraform/modules/ec2-instance/data.tf
+++ Terraform/modules/ec2-instance/data.tf
@@ -5,6 +5,9 @@ data "template_file" "user_data" {
   template = file("${path.module}/user_data.tpl")
 
   vars = {
+    aws_region        = var.region
+    bucket_name       = var.bucket_name
+    object_key        = "LoginWebApp.war"
     access_key        = var.access_key
     secret_access_key = var.secret_access_key
   }
